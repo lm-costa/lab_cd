ba3b7b4777e778f4463965633c21c61f54ecba20
lm-costa <luism.costa00@gmail.com>
2021-04-14 21:25:05 -0300

    2

--- test.R
+++ test.R
@@ -1,12 +1,12 @@
 #sempre usar para comitar no git
 # usethis::use_git()
 
-#usethis::use_github()
+usethis::use_github()
 
-usethis::create_github_token()
+#usethis::create_github_token()
+#usar sem nada dentro e somente colar o token
 
 # gitcreds::gitcreds_set()
-#c omentario
 
-mean(4,5,7,8,9)
+
 
