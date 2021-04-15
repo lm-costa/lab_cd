b1de3073a68ef64bac604cf4dcf56604aef1c209
lm-costa <luism.costa00@gmail.com>
2021-04-14 21:21:25 -0300

    second

--- test.R
+++ test.R
@@ -1,4 +1,12 @@
 #sempre usar para comitar no git
 # usethis::use_git()
 
-usethis::use_github()
+#usethis::use_github()
+
+usethis::create_github_token()
+
+# gitcreds::gitcreds_set()
+#c omentario
+
+mean(4,5,7,8,9)
+
